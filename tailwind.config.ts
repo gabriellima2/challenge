739c24ae6bd219192faa27dbc8f31023e92df3d5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-08 14:40:46 -0300

    chore: define base styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,30 @@ const config: Config = {
 	],
 	theme: {
 		extend: {
+			colors: {
+				background: '#F0F0F5',
+				text: {
+					primary: '#737380',
+					secondary: '#41414D',
+					tertiary: '#09090A',
+				},
+				brand: {
+					orange: '#FFA585',
+					blue: '#115D8C',
+				},
+				others: {
+					green: '#51B853',
+					red: '#DE3838',
+				},
+			},
+			fontSize: {
+				xl: '2rem',
+				lg: '1.5rem',
+				md: '1.25rem',
+				regular: '1rem',
+				sm: '0.875rem',
+				xs: '0.75rem',
+			},
 			fontFamily: {
 				primary: ['var(--font-primary)', 'sans-serif'],
 			},
