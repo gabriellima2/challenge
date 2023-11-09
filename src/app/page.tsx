35c62064be1649551c287c5d219d451aa2adb004
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-09 18:31:38 -0300

    feat: adds product listing with dynamic data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
-export default function Home() {
-	return <p className="font-primary">Hello</p>
+import { ProductList } from '@/ui/components'
+import { Error } from '@/ui/atoms'
+
+import { makeProductService } from '@/services/product.service'
+
+export default async function Home() {
+	const { data, error } = await makeProductService().getAll()
+	return (
+		<main>
+			<article>
+				{error && <Error>{error.message}</Error>}
+				{!error && <ProductList products={data} />}
+			</article>
+		</main>
+	)
 }
