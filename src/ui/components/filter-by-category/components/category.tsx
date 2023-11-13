7704a6e764b3f27ff5e777e8849997aed6375425
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-13 17:50:09 -0300

    fix: add li tag to listing in filter-by-category component

--- src/ui/components/filter-by-category/components/category.tsx
+++ src/ui/components/filter-by-category/components/category.tsx
@@ -12,19 +12,22 @@ type CategoryProps = Omit<
 export const Category = (props: CategoryProps) => {
 	const { text, isActive, ...rest } = props
 	return (
-		<button
-			{...rest}
-			type="button"
-			disabled={isActive}
-			className={cn(
-				'whitespace-nowrap border-b-4 bg-none font-normal uppercase',
-				isActive
-					? 'pointer-events-none border-brand-orange font-bold text-font-secondary'
-					: undefined,
-				rest.className
-			)}
-		>
-			{text}
-		</button>
+		<li>
+			<button
+				{...rest}
+				type="button"
+				disabled={isActive}
+				onSelect={(e) => console.log(e)}
+				className={cn(
+					'whitespace-nowrap border-b-4 bg-none font-normal uppercase',
+					isActive
+						? 'pointer-events-none border-brand-orange font-bold text-font-secondary'
+						: undefined,
+					rest.className
+				)}
+			>
+				{text}
+			</button>
+		</li>
 	)
 }
