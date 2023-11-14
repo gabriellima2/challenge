2fa6f10d6d7494beb0d11a86fe1f67aa13a3bb4d
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-14 16:19:51 -0300

    refactor: removes unnecessary files

--- src/dtos/base.dto.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloError } from '@apollo/client'
-
-export interface BaseDTO<T> {
-	data: T
-	error?: ApolloError | Error
-}
--- src/dtos/product.dto.ts
+++
@@ -1,4 +0,0 @@
-import { ProductEntity } from '@/entities/product.entity'
-import { BaseDTO } from './base.dto'
-
-export interface GetAllProductsDTO extends BaseDTO<ProductEntity[]> {}
