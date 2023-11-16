653ffcd311206b82b9341fbd55534c850d6f4361
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-16 19:49:50 -0300

    feat: centralizes the entire state in hook of home

--- src/ui/templates/home/home.tsx
+++ src/ui/templates/home/home.tsx
@@ -3,24 +3,21 @@ import { FilterProducts, OrderProducts, ProductsPagination } from './components'
 import { ProductList } from '@/ui/components'
 import { Error, Loading } from '@/ui/atoms'
 
-import { useProductsPagination } from './hooks/use-products-pagination'
-import { useProductsCategory } from './hooks/use-products-category'
-import { useOrderProducts } from './hooks/use-order-products'
-import { useGetProducts } from '@/hooks/use-get-products'
-
-import { ITEMS_PER_PAGE } from '@/constants/pagination'
+import { useHomeState } from './hooks/use-home-state'
 
 export const Home = () => {
-	const { category, onCategoryClick } = useProductsCategory()
-	const { currentPage, onPageClick } = useProductsPagination()
-	const { data, error, loading } = useGetProducts({
+	const {
+		products,
+		error,
+		loading,
 		category,
-		page: currentPage,
-		limit: ITEMS_PER_PAGE,
-	})
-	const { products, orderBy, onOrderByChange } = useOrderProducts(
-		data?.products.items || []
-	)
+		currentPage,
+		totalPages,
+		orderBy,
+		onCategoryClick,
+		onOrderByChange,
+		onPageClick,
+	} = useHomeState()
 	return (
 		<article className="flex flex-col gap-6">
 			<header className="flex flex-row flex-wrap justify-between gap-6">
@@ -29,7 +26,7 @@ export const Home = () => {
 			</header>
 			<ProductsPagination
 				currentPage={currentPage}
-				totalPages={data?.products.totalPages || 0}
+				totalPages={totalPages}
 				onClick={onPageClick}
 			/>
 			<main>
---
+++ src/ui/templates/home/hooks/use-home-state.ts
@@ -0,0 +1,31 @@
+import { useProductsPagination } from './use-products-pagination'
+import { useProductsCategory } from './use-products-category'
+import { useGetProducts } from '@/hooks/use-get-products'
+import { useOrderProducts } from './use-order-products'
+import { ITEMS_PER_PAGE } from '@/constants/pagination'
+
+export function useHomeState() {
+	const { category, onCategoryClick } = useProductsCategory()
+	const { currentPage, onPageClick } = useProductsPagination()
+	const { data, error, loading } = useGetProducts({
+		category,
+		page: currentPage,
+		limit: ITEMS_PER_PAGE,
+	})
+	const { products, orderBy, onOrderByChange } = useOrderProducts(
+		data?.products.items || []
+	)
+
+	return {
+		products,
+		error,
+		loading,
+		category,
+		currentPage,
+		totalPages: data?.products.totalPages || 0,
+		orderBy,
+		onPageClick,
+		onOrderByChange,
+		onCategoryClick,
+	}
+}
