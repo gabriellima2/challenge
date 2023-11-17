c93e516bf1577f74946a3c9bd9e822be9938f11f
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-17 18:34:49 -0300

    feat: define structure of products page

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,10 @@
+import { Products } from '@/ui/templates'
+
+type PageProps = {
+	params: { id: string }
+}
+
+export default function Page(props: PageProps) {
+	const { params } = props
+	return <Products />
+}
