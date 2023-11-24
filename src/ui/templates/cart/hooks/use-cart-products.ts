27f37b8de5babef1836954e850012445eee3dde5
Gabriel Lima <gabriel.rslima10@gmail.com>
2023-11-24 17:42:40 -0300

    style: improves readability of use-cart-products hook

--- src/ui/templates/cart/hooks/use-cart-products.ts
+++ src/ui/templates/cart/hooks/use-cart-products.ts
@@ -3,10 +3,16 @@ import { useMemo } from 'react'
 import { useGetProductsByIds } from '@/hooks/use-get-products-by-ids'
 import { useCartStore } from '@/store/cart-store'
 
+import type { CartStoreItem } from '@/store/cart-store/@types/cart-store-item'
+
+function getCartProductsIds(items: { [id: string]: CartStoreItem }) {
+	return Object.keys(items)
+}
+
 export function useCartProducts() {
 	const { items } = useCartStore((state) => state)
 	const { data, loading, error } = useGetProductsByIds({
-		ids: Object.keys(items),
+		ids: getCartProductsIds(items),
 	})
 
 	const products = useMemo(() => {
